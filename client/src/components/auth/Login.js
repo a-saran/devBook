130fa1feebdb9f49ff21cbc32e0f4c2f2394809c
asaran97 <asaran234@gmail.com>
2020-04-05 02:10:11 +0530

    login page and register page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const { email, password } = formData;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("success");
+  };
+
+  const onchange = ({ target: { name, value } }) =>
+    setFormData({ ...formData, [name]: value });
+
+  return (
+    <div className="card sign-in">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>
+        Sign Into Your Account
+      </p>
+      <form onSubmit={onSubmit} className="form">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={onchange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            name="password"
+            value={password}
+            onChange={onchange}
+          />
+        </div>
+        <input type="submit" value="Log In" className="btn-primary m-auto" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Register() {
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("passowrd do not match");
+    } else {
+      console.log("register");
+    }
+  };
+
+  const onchange = ({ target: { name, value } }) =>
+    setFormData({ ...formData, [name]: value });
+
   return (
-    <div>
-      Register
+    <div className="card register">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Create Your Account
+      </p>
+      <form onSubmit={onSubmit} className="form">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onchange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={onchange}
+            isrequired="true"
+          />
+          <small className="form-text">
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            name="password"
+            value={password}
+            onChange={onchange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm assword"
+            minLength="6"
+            name="password2"
+            value={password2}
+            onChange={onchange}
+          />
+        </div>
+        <input type="submit" value="Register" className="btn-primary" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
+
+export default Register;
