d823526f590a5f6724a7d527caaab2db15f7e579
asaran97 <asaran234@gmail.com>
2020-04-25 12:21:41 +0530

    production postbuild script and static file path allocation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require("dotenv");
+const path = require('path');
 
 const app= express();
 
@@ -13,13 +14,22 @@ app.use(express.json({ extended: false}))
 
 const PORT = process.env.PORT ||  5000;
 
-app.get('/', (req,res) => res.send('API running'))
+// app.get('/', (req,res) => res.send('API running'))
 
 //define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//serve static assets in production
+if(process.env.NODE_ENV = 'production') {
+  app.use(express.static('client/build'))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log('App listening on port 5000!');
