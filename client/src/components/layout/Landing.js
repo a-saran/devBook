05bb6eab581f6357bb4b530b7e802cafd3a91ff8
asaran97 <asaran234@gmail.com>
2020-09-15 09:52:05 +0530

    removed dummy text

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -18,12 +18,6 @@ const Landing = ({ isAuthenticated, user }) => {
             Create developer profile/porfolio,share posts and get help from
             other developers
           </h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
-            exercitationem molestiae, magnam repudiandae dolore libero
-            similique, illo beatae labore molestias, dicta id reiciendis error
-            optio nulla! Ea illum laborum non!
-          </p>
           <div className="buttons">
             <Link to="/register" className="btn-primary">Register</Link>
             <Link to="/login" className="btn-light">Sign In</Link>
